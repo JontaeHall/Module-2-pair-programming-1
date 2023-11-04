21b9ba56d44ee93d2da0ca5c00a1e4510ff6a9a4
Jontae <jontae327@gmail.com>
2023-11-04 17:27:28 -0400

    Pair programming 01 complete.

--- step-4.sql
+++ step-4.sql
@@ -1,15 +1,24 @@
 -- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
 -- (expected: 7 rows, starting with "Blackwoods")
-
+select park.name, campground.name, campground.open_from_mm, campground.open_to_mm, campground.daily_fee from campground
+right join park on park.park_id = campground.park_id
+order by park.name , campground.name;
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
 -- (expected: 3 rows, starting with "Acadia")
-
+select par.name, count(c.campground_id) from park par
+join campground c on par.park_id = c.park_id
+group by par.name 
+order by par.name;
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
 -- (expected: 12 rows)
-
+select  par.name, c.name, s.site_number, s.max_occupancy, s.accessible, s.max_rv_length, s.utilities from site s
+join campground c on s.campground_id = c.campground_id
+join park par on c.park_id = par.park_id
+where c.name = 'Blackwoods';
 
 -- select site number, reservation name, reservation from and to date ordered by reservation from date
 -- (expected: 44 rows, starting with the earliest date)
-
+select site_id, name, from_date, to_date from reservation 
+order by from_date;
--- step-5.sql
+++ step-5.sql
@@ -9,11 +9,12 @@
   description: Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.
   ------------------------------
 */
-
+insert into park (name, location, establish_date, area, visitors, description)
+values ('Ohiopyle State Park', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
-
+update park set visitors = 1500000;
 */
 
 
@@ -27,8 +28,8 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
-
-
+insert into campground (park_id, name, open_from_mm, open_to_mm, daily_fee)
+values(4, 'Youghiogheny', 01, 12, 95.00);
 /*
  STEP FOUR: Insert 3 new sites with the following data:
  ------------------------------------------------------------
@@ -37,6 +38,14 @@
   site_number: 625, campground_id: (use the id of the new campground you just added)
  ------------------------------------------------------------
 */
+insert  into site (site_number, campground_id)
+values(623, 8);
+
+insert into site (site_number, campground_id)
+values(624, 8);
+
+insert into site (site_number, campground_id)
+values (625, 8);
 
 
 /*
@@ -47,16 +56,26 @@
   site_id: (the site_id for site number 625 that you just created), name: Kent Family, from_date: today + 12 days, to_date: today + 20 days
 ------------------------------------------------------------------------------------
 */
+insert into reservation ( site_id, name, from_date, to_date)
+values (52, 'Wayne Family',  '2023-11-14', '2023-11-24');
 
+insert into reservation (site_id, name, from_date, to_date)
+values (53, 'Parker Family', '2023-11-15', '2023-11-24');
+ 
+insert into reservation (site_id, name, from_date, to_date)
+values (54, 'Kent Family', '2023-11-16', '2023-11-24');
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
-
-
+update reservation set from_date = '2023-11-04' , to_date = '2023-11-11'
+where name = 'Wayne Family';
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
 
 */
+delete  from reservation 
+where name = 'Kent Family';
+select * from reservation;
 
